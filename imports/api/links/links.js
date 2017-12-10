c91512b458fb494e47e8679d9ae72c9156b01a05
Steven Dao <iamstevendao@users.noreply.github.com>
2017-12-10 20:43:26 +1100

    add header and footer

--- imports/api/links/links.js
+++ imports/api/links/links.js
@@ -1,5 +1,3 @@
-// Definition of the links collection
-
 import { Mongo } from 'meteor/mongo';
 
 export const Links = new Mongo.Collection('links');
--- imports/startup/client/app.vue
+++ imports/startup/client/app.vue
@@ -1,5 +1,17 @@
 <template lang="pug">
 body
+  include ../../ui/includes/header
   router-view
+  include ../../ui/includes/footer
 </template>
+
+<style scoped>
+footer {
+  width: 100%;
+  position: absolute;
+  bottom: 0;
+  text-align: right;
+  padding: 0.5em 1em;
+}
+</style>
 
